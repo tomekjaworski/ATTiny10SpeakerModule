7d6829573468b6af44eaf44a474f67f2e272bdea
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-07-31 11:19:34 +0300

    Empty project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ATTiny10SpeakerModule.c
+ *
+ * Created: 31.07.2017 11:14:31
+ * Author : terminal
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
